dbd172f41ffcca156aa2369d21d0571a8e65436c
wangqingpeng <wangqingpeng@baidu.com>
2022-09-25 00:05:59 +0800

    add dl4jweb

--- dl4jcookbook/classification/src/main/java/top/sailingsan/dl4j/cookbook/SpringDl4jApplication.java
+++ dl4jcookbook/classification/src/main/java/top/sailingsan/dl4j/cookbook/SpringDl4jApplication.java
@@ -1,25 +1,13 @@
 package top.sailingsan.dl4j.cookbook;
 
-import java.io.File;
-import java.io.IOException;
-
-import org.apache.commons.io.FileUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.util.Base64Utils;
 
 @SpringBootApplication
 public class SpringDl4jApplication {
 
     public static void main(String[] args) {
-        try {
-            System.out.println(Base64Utils.encodeToString(FileUtils.readFileToByteArray(new File(
-                        "/Users/wangqingpeng/git/github/hello/dl4jcookbook/facereid/src/main/resources/b2.jpg"))));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-//        SpringApplication.run(SpringDl4jApplication.class, args);
+        SpringApplication.run(SpringDl4jApplication.class, args);
     }
 
 }
---
+++ docs/javacpp-hello.md
@@ -0,0 +1,12 @@
+### env
+
+系统: macos monterey V12.2 
+
+芯片： Apple M1 Pro
+
+jdk： azul_1.8.0_332 x86 
+
+javacpp: 1.5.7
+
+
+
